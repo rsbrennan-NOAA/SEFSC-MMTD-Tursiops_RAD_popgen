83c9824712ca038eb0cd39ceef604be4b4f3af7d
rsbrennan-NOAA <reid.brennan@noaa.gov>
2025-04-07 15:39:25 -0700

    filtering

--- scripts/data_processing/filter.2.sh
+++ scripts/data_processing/filter.2.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-type=END
 #SBATCH -o %x_%j.out
 #SBATCH -e %x_%j.err
-#SBATCH -D /home/rbrennan/Tursiops-NC-PopulationAssignment-RAD/logout
+#SBATCH -D /home/rbrennan/Tursiops-RAD-popgen/logout
 #SBATCH -n 1
 #SBATCH --mem=16G
 #SBATCH --partition=standard
@@ -16,18 +16,19 @@ mamba activate vcflib-1.0.9
 module load bio/vcftools/0.1.16
 
 INDIR=~/Tursiops-NC-PopulationAssignment-RAD/analysis/variants
+OUTDIR=~/Tursiops-RAD-popgen/analysis/variants/
 
-vcftools --gzvcf ${INDIR}/filtered.3.vcf.gz --remove ~/Tursiops-NC-PopulationAssignment-RAD/scripts/rm_missing.txt  --recode --recode-INFO-all --stdout |  bgzip > ${INDIR}/filtered.4.vcf.gz 
+vcftools --gzvcf ${INDIR}/filtered.3.vcf.gz --remove ~/Tursiops-RAD-popgen/scripts/rm_missing.txt  --recode --recode-INFO-all --stdout |  bgzip > ${OUTDIR}/filtered.4.vcf.gz 
 
-vcftools --gzvcf ${INDIR}/filtered.4.vcf.gz  --max-missing 0.7 --maf 0.05 --min-meanDP 10 --recode --recode-INFO-all  --stdout  >  ${INDIR}/filtered.5.vcf
+vcftools --gzvcf ${OUTDIR}/filtered.4.vcf.gz  --max-missing 0.7 --maf 0.05 --min-meanDP 10 --recode --recode-INFO-all  --stdout  >  ${OUTDIR}/filtered.5.vcf
 
-NUM_VARIANTS1=$(cat ${INDIR}/filtered.5.vcf | grep -v '^#' | wc -l)
+NUM_VARIANTS1=$(cat ${OUTDIR}/filtered.5.vcf | grep -v '^#' | wc -l)
 echo "Number of variants after missing and maf filters: ${NUM_VARIANTS1}"
 
-vcffilter -s -f "AB > 0.25 & AB < 0.75 | AB < 0.01" ${INDIR}/filtered.5.vcf |bgzip > ${INDIR}/filtered.6.vcf.gz
+vcffilter -s -f "AB > 0.25 & AB < 0.75 | AB < 0.01" ${OUTDIR}/filtered.5.vcf |bgzip > ${OUTDIR}/filtered.6.vcf.gz
 
-NUM_VARIANTS2=$(zcat ${INDIR}/filtered.6.vcf.gz | grep -v '^#' | wc -l)
+NUM_VARIANTS2=$(zcat ${OUTDIR}/filtered.6.vcf.gz | grep -v '^#' | wc -l)
  echo "Number of variants after AB  filters: ${NUM_VARIANTS2}"
 
 #depths
-vcftools --gzvcf ${INDIR}/filtered.6.vcf.gz --site-mean-depth --out ${INDIR}/filtered.6.depth
+vcftools --gzvcf ${OUTDIR}/filtered.6.vcf.gz --site-mean-depth --out ${OUTDIR}/filtered.6.depth
