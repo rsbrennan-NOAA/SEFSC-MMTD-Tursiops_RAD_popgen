1e6ff5d1c836b3fbbe85ecaede1c138813df3335
rsbrennan-NOAA <reid.brennan@noaa.gov>
2025-04-08 16:05:03 -0700

    admixture

--- scripts/admixture.sh
+++ scripts/admixture.sh
@@ -12,24 +12,34 @@
 #SBATCH --time=03:00:00
 
 
+source ~/.bashrc
+
+mamba activate vcflib-1.0.9
 module load  bio/admixture/1.3.0
 module load bio/vcftools/0.1.16
 module load bio/plink/1.90b6.23
 
 cd ~/Tursiops-RAD-popgen/analysis/pop_structure
 
-zcat ~/Tursiops-RAD-popgen/analysis/variants/filtered.final_ids_LDthin.vcf.gz| \
+# drop sex chr
+vcftools --gzvcf ~/Tursiops-RAD-popgen/analysis/variants/filtered.final_ids_LDthin.vcf.gz --not-chr NC_047055.1 --recode --recode-INFO-all --stdout |  bgzip > ~/Tursiops-RAD-popgen/analysis/variants/filtered.final_ids_LDthin_noX.vcf.gz
+
+
+zcat ~/Tursiops-RAD-popgen/analysis/variants/filtered.final_ids_LDthin_noX.vcf.gz| \
 grep -v "^##contig=<ID=N[CW]_"  | awk -F'\t' 'BEGIN {OFS="\t"} 
     /^#/ {print; next} 
-    {split($1, a, /\./); sub(/^N[CW]_/, "", a[1]); $1 = a[1]; print}' > ~/Tursiops-RAD-popgen/analysis/variants/LDthin_numCorrect.vcf
+    {split($1, a, /\./); sub(/^N[CW]_/, "", a[1]); $1 = a[1]; print}' > ~/Tursiops-RAD-popgen/analysis/variants/LDthin_noX_numCorrect.vcf
 
-plink --vcf ~/Tursiops-RAD-popgen/analysis/variants/LDthin_numCorrect.vcf \
---make-bed --out ~/Tursiops-RAD-popgen/analysis/variants/LDthin_numCorrect \
+plink --vcf ~/Tursiops-RAD-popgen/analysis/variants/LDthin_noX_numCorrect.vcf \
+--make-bed --out ~/Tursiops-RAD-popgen/analysis/variants/LDthin_noX_numCorrect \
 --allow-extra-chr --double-id
 
 
+rm log*.out
+
 for K in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; \
-do admixture -j4 -s time --cv ~/Tursiops-RAD-popgen/analysis/variants/LDthin_numCorrect.bed $K | tee log${K}.out; done
+do admixture -j4 -s time --cv ~/Tursiops-RAD-popgen/analysis/variants/LDthin_noX_numCorrect.bed $K | tee log${K}.out; done
+
 
 grep -h CV log*.out | cut -f 3- -d " " > cv.txt
 
--- scripts/data_processing/filter.3.sh
+++ scripts/data_processing/filter.3.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-type=END
 #SBATCH -o %x_%j.out
 #SBATCH -e %x_%j.err
-#SBATCH -D /home/rbrennan/Tursiops-NC-PopulationAssignment-RAD/logout
+#SBATCH -D /home/rbrennan/Tursiops-RAD-popgen/logout
 #SBATCH -n 1
 #SBATCH --mem=16G
 #SBATCH --partition=standard
@@ -16,12 +16,17 @@ mamba activate vcflib-1.0.9
 module load bio/vcftools/0.1.16
 module load bio/bcftools/1.11
 
-INDIR=~/Tursiops-NC-PopulationAssignment-RAD/analysis/variants
+INDIR=~/Tursiops-RAD-popgen/analysis/variants
 
-vcftools --gzvcf ${INDIR}/filtered.6.vcf.gz --max-meanDP 139 --exclude-positions ~/Tursiops-NC-PopulationAssignment-RAD/scripts/HD_exclude.txt --recode --recode-INFO-all --stdout |  bgzip > ${INDIR}/filtered.final.vcf.gz
+vcftools --gzvcf ${INDIR}/filtered.6.vcf.gz --max-meanDP 136 --exclude-positions ~/Tursiops-RAD-popgen/scripts/HD_exclude.txt --recode --recode-INFO-all --stdout |  bgzip > ${INDIR}/filtered.final_MitoIncluded.vcf.gz
+
+# remove the mitochondrial chromosome
+
+vcftools --gzvcf ${INDIR}/filtered.final_MitoIncluded.vcf.gz --not-chr NC_012059.1 --recode --recode-INFO-all --stdout |  bgzip > ${INDIR}/filtered.final.vcf.gz
 
 bcftools annotate --set-id '%CHROM\_%POS'  ${INDIR}/filtered.final.vcf.gz  -Oz -o ${INDIR}/filtered.final_ids.vcf.gz
 
+bcftools index -t ${INDIR}/filtered.final_MitoIncluded.vcf.gz
 bcftools index -t ${INDIR}/filtered.final.vcf.gz
 bcftools index -t ${INDIR}/filtered.final_ids.vcf.gz
 
