27410b664b83faf8b8ef7c4b0d3614d495a0a262
rsbrennan-NOAA <reid.brennan@noaa.gov>
2025-04-10 13:04:33 -0700

    plink distance

--- scripts/tursiops_aduncus_intersect.sh
+++ scripts/tursiops_aduncus_intersect.sh
@@ -10,6 +10,10 @@
 #SBATCH --partition=standard
 #SBATCH --time=1:00:00
 
+source ~/.bashrc
+
+mamba activate vcflib-1.0.9
+
 module load bio/vcftools/0.1.16
 module load bio/plink/1.90b6.23
 module load bio/htslib/1.19
@@ -17,13 +21,21 @@ module load bio/bcftools/1.11
 
 cd ~/Tursiops-RAD-popgen/analysis/variants
 
+# remove x chrom
+
+vcftools --gzvcf ~/Tursiops-RAD-popgen/analysis/variants/filtered.final_ids.vcf.gz --not-chr NC_047055.1 --recode --recode-INFO-all --stdout |  bgzip > ~/Tursiops-RAD-popgen/analysis/variants/filtered.final_ids_noX.vcf.gz
+
+tabix filtered.final_ids_noX.vcf.gz
+
 # get rad positions
-bcftools query -f '%CHROM\t%POS\n' filtered.final_ids.vcf.gz > rad_positions.txt
+bcftools query -f '%CHROM\t%POS\n' filtered.final_ids_noX.vcf.gz > rad_positions.txt
+
 # keep only rad positions
 bcftools view -R rad_positions.txt ~/Tursiops-RAD-popgen/analysis/variants/aduncus/aduncus.vcf.gz |bgzip > ~/Tursiops-RAD-popgen/analysis/variants/aduncus/aduncus_radsites.vcf.gz
 tabix ~/Tursiops-RAD-popgen/analysis/variants/aduncus/aduncus_radsites.vcf.gz
+
 #merge
-bcftools merge ~/Tursiops-RAD-popgen/analysis/variants/aduncus/aduncus_radsites.vcf.gz filtered.final_ids.vcf.gz|bgzip > tursiops_aduncus_1.vcf.gz
+bcftools merge ~/Tursiops-RAD-popgen/analysis/variants/aduncus/aduncus_radsites.vcf.gz filtered.final_ids_noX.vcf.gz | bgzip > tursiops_aduncus_1.vcf.gz
 tabix tursiops_aduncus_1.vcf.gz
 
 zcat tursiops_aduncus_1.vcf.gz | grep -v '^#' | wc -l
