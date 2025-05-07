6b2266f35d194765de9b9277c6a04d6e82794336
rsbrennan-NOAA <reid.brennan@noaa.gov>
2025-05-07 13:49:19 -0700

    submit and organize stairwayplot

---
+++ scripts/demography/stairwayPlot/stairwayPlot_fourpop_CGulf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=SP_CGulf
+#SBATCH --mail-user=reid.brennan@noaa.gov
+#SBATCH --mail-type=END
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+#SBATCH -D /home/rbrennan/Tursiops-RAD-popgen/logout
+#SBATCH --mem=32G
+#SBATCH --partition=standard
+#SBATCH --time=99:00:00
+
+cd ~/Tursiops-RAD-popgen/analysis/moments/stairway_plot_v2.1.1
+
+BPPrefix=fourpop_CGulf
+
+echo "Population is $BPPrefix"
+
+java -cp stairway_plot_es Stairbuilder ${BPPrefix}.blueprint
+
+bash ${BPPrefix}.blueprint.sh 
